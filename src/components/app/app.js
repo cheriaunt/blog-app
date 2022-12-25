fd8a4799c09b11494d09723c9e48c580a51bae54
cheriaunt <alyonakravetsdenisovna@gmail.com>
2022-12-25 17:24:51 +0600

    create react app

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './app.module.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
